4b752b6203e5f940546a58052db66c9b7baa0c1a
Patricio Miguel Osorio Osorio <patricio.osorio@agilityps.mx>
2026-03-04 10:22:44 -0600

    Sección 2: Bases de Zustand - Custom Storage - SessionStorage

--- src/stores/person/person.store.ts
+++ src/stores/person/person.store.ts
@@ -1,7 +1,8 @@
 import { create, StateCreator } from 'zustand';
-import { persist } from 'zustand/middleware';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
 import { LOCAL_STORAGE_KEY } from '@utils/constants';
+import { StateStorage } from 'zustand/middleware';
 
 export interface IPerson {
   firstName: string;
@@ -23,8 +24,24 @@ const personStoreApi: StateCreator<IPersonState> = (set) => ({
   setLastName: (value) => set((state) => ({ lastName: value })),
 });
 
+const sessionStorage: StateStorage = {
+  getItem: function (name: string): string | null | Promise<string | null> {
+    console.log(`getItem: `, name);
+    throw new Error('Function not implemented.');
+  },
+  setItem: function (name: string, value: string): unknown {
+    console.log(`setItem: `, name, value);
+    throw new Error('Function not implemented.');
+  },
+  removeItem: function (name: string): unknown {
+    console.log(`removeItem: `, name);
+    throw new Error('Function not implemented.');
+  },
+};
+
 export const usePersonState = create<IPersonState>()(
   persist(personStoreApi, {
     name: LOCAL_STORAGE_KEY.PERSON,
+    storage: createJSONStorage(() => sessionStorage),
   })
 );
